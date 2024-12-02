be43e3f87e636a672cfd9cb4251b7bedc245cf49
Tavera15 <joaquinramirez97@yahoo.com>
2024-12-01 18:22:32 -0600

    Adding favicons and app name

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,47 @@ import { VitePWA } from "vite-plugin-pwa";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), VitePWA()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
 })
+
+
+const manifestForPlugin = {
+  registerType:'prompt',
+  includeAssests:['favicon.ico', "apple-touc-icon.png", "masked-icon.svg"],
+  manifest:{
+    name:"Joaquin Business Select Price Comparison",
+    short_name:"JRT BizzSel",
+    description:"An App to compare Regular Price vs Business Select Price at Office Depot",
+    icons:[{
+      src: '/JRT-BizSel.png',
+      sizes:'192x192',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src:'/JRT-BizSel.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'favicon'
+    },
+    {
+      src: '/JRT-BizSel.png',
+      sizes:'180x180',
+      type:'image/png',
+      purpose:'apple touch icon',
+    },
+    {
+      src: '/JRT-BizSel.png',
+      sizes:'512x512',
+      type:'image/png',
+      purpose:'any maskable',
+    }
+  ],
+  theme_color:'#171717',
+  background_color:'#f0e7db',
+  display:"standalone",
+  scope:'/',
+  start_url:"/",
+  orientation:'portrait'
+  }
+}
